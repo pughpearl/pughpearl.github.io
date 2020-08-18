11d1b3bdb4809289806eee5657de7e0240b033cf
Ashish Mishra <ashish.mishra@lowes.com>
2020-08-18 16:36:18 +0530

    adding table for multiple orders

--- js/shop-form.js
+++ js/shop-form.js
@@ -40,20 +40,59 @@ $("#is-shipping-same").change(function() {
 		});		
 });
 
-$(document).ready(function(){
-	$(".checkout").on("keyup", ".quantity", function(){
-		var price = +$(this).data("price");
+$(document).on('change','.item-table',function(){
+
+  var lookup = {
+    'Option 1': ['S','M','L','XL','2XL'],
+    'Option 2': ['S','M','L','XL','2XL'],
+    'Option 3': ['S','M','L'],
+    'Option 4': ['0-3','3-6','6-9','9-12']
+  };
+    var selectValues = $(this).val();
+    var selectValue = selectValues.split('-')[0];
+    var id =selectValues.split('-')[1];
+    $('#'+id).empty();
+    for (i = 0; i < lookup[selectValue].length; i++) {
+      $('#'+id).append("<option value='" + lookup[selectValue][i] + "'>" + lookup[selectValue][i] + "</option>");
+    }
+    var newId = id.slice(-1);
+    $("#table-total-"+newId).text('0');
+    $("#table-qty-input-"+newId).val(0);
+    
+});
+
+$(document).on('keyup','.quantity-table',function(){ 
     var quantity = +$(this).val();
-    var totalId = $(this).data("totalid");
-    var totalPrice  = (price * quantity).toFixed(2);
-    $("#"+totalId).text(totalPrice);
-    updateTotalPrice();
+    var id = $(this).data("productid");
+    var productid = $('#'+id).val();
+    if(productid){
+      var selectValue = productid.split('-')[0];
+      var pricing = {
+        'Option 1': 28,
+        'Option 2': 27,
+        'Option 3': 23,
+        'Option 4': 20
+      };
+      var price = pricing[selectValue];
+      var totalTable = (price * quantity).toFixed(2);
+      $("#table-total-"+id).text(totalTable);
+      var tableSubtotal = parseFloat($('#table-subtotal-hidden').val()) + parseFloat(totalTable);
+      $('#table-subtotal-hidden').val(tableSubtotal);
+      updateTotalPrice();
+      $('#more-order-id-'+id).val('More Order::: Type : '+$('#'+id+' option:selected').text()+', Size:'+ $('#choices'+id).val()+
+                ',  Quantity: '+quantity);
+
+    }else{
+      alert('Please Select the T-shirt Type and Size!');
+    }
+
   })
+
   function updateTotalPrice(){
     var subTotal = parseFloat($('#total-signed').html()) + parseFloat($('#total-nonsigned').html()) +
                     parseFloat($('#total-mask').html()) + parseFloat($('#total-men-tshirt').html()) + 
                     parseFloat($('#total-women-tshirt').html()) + parseFloat($('#total-youth-tshirt').html()) +
-                    parseFloat($('#total-onesies-tshirt').html());
+                    parseFloat($('#total-onesies-tshirt').html()) + parseFloat($('#table-subtotal-hidden').val());
    if(subTotal>0){
       var taxes = parseFloat(0.06*subTotal).toFixed(2);
       var total = (parseFloat(subTotal) + parseFloat(taxes) + parseFloat(8.25)).toFixed(2);
@@ -70,5 +109,54 @@ $(document).ready(function(){
       $('#order-total').val('SubTotal: $0,  Tax: 0$, Shipping Fees: $0, Total: $0');
     }
   }
+$(document).ready(function(){
 
+	$(".checkout").on("keyup", ".quantity", function(){
+		var price = +$(this).data("price");
+    var quantity = +$(this).val();
+    var totalId = $(this).data("totalid");
+    var totalPrice  = (price * quantity).toFixed(2);
+    $("#"+totalId).text(totalPrice);
+    updateTotalPrice();
+  })
+
+  $(function () {
+    var rowCount=0;
+    $("#add-more").click(function(){
+      
+      rowCount++;
+
+      var div = $("<tr />");
+        div.html(GetDynamicTextBox(rowCount));
+        $("#TextBoxContainer").append(div);
+      
+    });
+    $("body").on("click", ".remove", function () {
+        var id = $(this).data("removeid");
+        var presentTotal = $("#table-total-"+id).html();
+        var tableSubtotal = parseFloat($('#table-subtotal-hidden').val()) - parseFloat(presentTotal);
+        $('#table-subtotal-hidden').val(tableSubtotal);
+        updateTotalPrice();
+        $(this).closest("tr").remove();
+    });
+});
+function GetDynamicTextBox(value) {
+    return '<td><select id="'+value+'" class="item-table">'+
+    '<option value="" selected>Select</option>'+
+    '<option value="Option 1-choices'+value+'">Men</option>'+
+    '<option value="Option 2-choices'+value+'">Women</option>'+
+    '<option value="Option 3-choices'+value+'">Youth</option>'+
+    '<option value="Option 4-choices'+value+'">Onesie</option>'+
+    '</select>'+
+    '</td>'+
+    '<td>'+
+    '<select id="choices'+value+'">'+
+    '<option value="" disabled selected>Select</option>'+
+    '</select>'+
+    '</td>'+ '<td><input type="number" id="table-qty-input-'+value+'" data-productid="'+value+'" class="quantity-table" value="0"/></td>'
+    + '<input type="hidden" id="more-order-id-'+value+'" name="more-order-'+value+'" value=""/>'
+    + '<td>$<span id="table-total-'+value+'">0</span></td>'+
+      '<td><button type="button" data-removeid="'+value+'" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
+}
+ 
 })
